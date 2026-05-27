ebee6676afaaba1bbef63ef18728c2eea755f012
TanishKumar984 <167670361+TanishKumar984@users.noreply.github.com>
2026-05-27 07:22:30 +0000

    01_variable learn

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 123422
+let accountEmail = "tanish@123gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+
+// accountId = 2 is not allowed
+accountEmail = "tanish432@gmail.com"
+accountPassword = "546"
+accountCity = "Katra"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState]);
