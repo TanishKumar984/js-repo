374b73a55320c37644a9478d9149c2efba8c95ae
TanishKumar984 <167670361+TanishKumar984@users.noreply.github.com>
2026-05-27 15:30:56 +0000

    Learn heap and stack

---
+++ 01_basics/06_heap.js
@@ -0,0 +1,18 @@
+// Stack(primitive) , Heap(non-primitiv090)
+
+let youtubechannel = "tanishkumar";
+
+let anothername = youtubechannel;
+anothername = "kartik"
+
+console.log(anothername);
+console.log(youtubechannel);
+
+let userone = {
+    name: "Tanish Kumar",
+    age : 21,
+}
+let usertwo = userone
+usertwo.age = 24
+console.log(userone);
+console.log(usertwo.age);
