13b117eb8f8b97fe90d195e5d3f316aaefb8d37c
TanishKumar984 <167670361+TanishKumar984@users.noreply.github.com>
2026-05-27 10:14:39 +0000

    learn conversionoperation

---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = true;
+
+// console.log(typeof score);
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let str1 = "hello"
+let str2 = "Tanish"
+let str3 = str1 +" "+ str2
+console.log(str3);
+
+console.log("1" + 2); 
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
