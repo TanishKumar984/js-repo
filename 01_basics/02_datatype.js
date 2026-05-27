acd6cece4e1e60d7f71eb4e207d2f18c87e6128d
TanishKumar984 <167670361+TanishKumar984@users.noreply.github.com>
2026-05-27 08:04:34 +0000

    work on datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,18 @@
+let age = 18
+let isloggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// null => standalone value
+//  undefined =>
+// symbol => unique
+
+// object
+
+
+
+console.log(typeof(age))
+console.table(typeof(isloggedIn))
+console.log(typeof(state))
