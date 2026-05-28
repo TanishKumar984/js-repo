32eedaad3531c59e17e1bd5e8945d49665b95c2d
TanishKumar984 <167670361+TanishKumar984@users.noreply.github.com>
2026-05-28 06:42:00 +0000

    learn maths and nums

---
+++ 01_basics/08_nums_and_math.js
@@ -0,0 +1,11 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+// ++++++++++++++maths++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.max(23,4,6,7,8));
+console.log(Math.min(3,4,5,67,7,8));
