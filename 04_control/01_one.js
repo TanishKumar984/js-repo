1aac30657a9d05acc28c64e63d628baf28a6062b
TanishKumar984 <167670361+TanishKumar984@users.noreply.github.com>
2026-05-29 08:24:20 +0000

    learn control

---
+++ 04_control/01_one.js
@@ -0,0 +1,28 @@
+// if
+const isUserloggedIn = true
+
+// if(2 !== 2){
+//    console.log("executed");
+// }
+// else {
+//     console.log(" false");
+// }
+
+
+// const balance = 100;
+
+// if(balance > 100){
+//     console.log("its good")
+// } else if(balance == 100){
+//     console.log("you are on line");
+// }
+// else{
+//     console.log("its not good ");
+// }
+
+const userLoggedIn = true
+const debitcard = true 
+
+if(userLoggedIn &&debitcard){
+    console.log("allow to buy course");
+}
